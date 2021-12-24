d616ac10dc96c524d7222cad2bd0047e04f3b55e
Rory Hemmings <rpmhemmings@gmail.com>
2021-12-23 22:58:20 -0800

    added image class (#1)

--- src/image.h
+++ src/image.h
@@ -1,11 +1,50 @@
 #ifndef TIV_IMAGE_H
 #define TIV_IMAGE_H
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include <string>
+
+enum IMAGE_MODE
+{
+	REGULAR,
+	REGULAR_SHADED,
+	REGULAR_COLOR,
+	BLOCK_SHADED,
+	BLOCK_COLOR
+};
+
+// Window forward declaration
+class Window;
+
 class Image
 {
 	public:
+			// Constructor
+		Image(Window* window, 
+					const std::string& path, 
+					int maxWidth, 
+					int maxHeight, 
+					IMAGE_MODE mode=REGULAR);
+
+			// Mutators	
+		void Resize(double widthScale, heightScale);
+
+			// Accessors
+		void 			 Display() const;
+		int  			 GetWidth() const;
+		int  			 GetHeight() const;
+		IMAGE_MODE GetMode() const { return m_mode };
 
 	private:
+		Window* m_window;
+		Mat m_img;
+
+		IMAGE_MODE m_mode;
+
+		double m_widthScale;
+		double m_heightScale;
 
 };
 
